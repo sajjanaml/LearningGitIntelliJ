e0ce1381e1fdbaab64682dc1d9ec42e1d7dd5059
laxmanlearning <98250780+laxmanlearning@users.noreply.github.com>
2022-01-23 14:39:39 +0530

    Added -*** (#3)
    Added -***

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,9 +1,10 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass-Learn-123-abc");
+        System.out.println("DevClass-Learn-123-abc-***");
         //Laxman Added This
         //Checkin from sajjanaml
         //Adding + ABC
         //Changing +abc to -abc
+        //Added -***
     }
 }
